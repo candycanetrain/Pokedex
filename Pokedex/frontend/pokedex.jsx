9065503963bff2bf996f4d2d9d29b78e90903838
Candra_Tran <candratran@gmail.com>
2017-03-08 15:31:25 -0800

    finished phase 2

--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/api_util.js';
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions.js';
+import configureStore from './store/store.js';
+import selectAllPokemon from './reducers/selectors';
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
+  window.store = store;
+  window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
 });
---
+++ Pokedex/frontend/reducers/selectors.js
@@ -0,0 +1,5 @@
+import values from 'lodash/values';
+const selectAllPokemon = (state) => {
+  return values(state.pokemon);
+};
+export default selectAllPokemon;
