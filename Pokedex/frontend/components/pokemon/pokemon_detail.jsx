5cb6f2547059663a41a333449757f85b5488bea4
Candra_Tran <candratran@gmail.com>
2017-03-09 16:42:08 -0800

    Add Phase V

--- Pokedex/frontend/components/pokemon/pokemon_detail.jsx
+++ Pokedex/frontend/components/pokemon/pokemon_detail.jsx
@@ -5,6 +5,11 @@ class PokemonDetail extends React.Component {
   componentDidMount(){
     this.props.requestPoke(this.props.params.Id);
   }
+
+  componentWillReceiveProps(newProps){
+    this.props.requestPoke(newProps.params.Id);
+  }
+
   render() {
 
     return (
@@ -14,7 +19,13 @@ class PokemonDetail extends React.Component {
         <li>Defense: {this.props.pokemonDetails.defense}</li>
         <li>Id: {this.props.pokemonDetails.id}</li>
         <li><img src={this.props.pokemonDetails.image_url}/></li>
-        
+        <li>Moves:
+          <ul>
+            {this.props.pokemonDetails.moves.map((move, idx) => (
+              <li key={idx}>{move}</li>
+            ))}
+          </ul>
+        </li>
         <li>PokeType: {this.props.pokemonDetails.poke_type}</li>
 
 
